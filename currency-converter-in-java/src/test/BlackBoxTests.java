a4bf1e86ac7589791ea69be3df319b46d53d631c
Benliam12 <s100live.ca@live.ca>
2023-12-05 19:40:18 -0500

    Deuxième test blackbox

--- currency-converter-in-java/src/test/BlackBoxTests.java
+++ currency-converter-in-java/src/test/BlackBoxTests.java
@@ -2,18 +2,25 @@ package test;
 
 import currencyConverter.Currency;
 import currencyConverter.MainWindow;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BlackBoxTests {
 
     private ArrayList<Currency> currencies = Currency.init();
 
+    /**
+     * We test every single pair of currencies.
+     * @param c1 Currency 1
+     * @param c2 Currency 2
+     */
     @ParameterizedTest
     @MethodSource("currencyCheck")
     void testCurrencyConvertingList(String c1, String c2){
@@ -21,6 +28,10 @@ public class BlackBoxTests {
         assertTrue(MainWindow.convert(c1,c2, currencies, 1d) > 0d);
     }
 
+    /**
+     * Generates every possible currency trading.
+     * @return List of every possible currency trading needed for the test.
+     */
     static Stream<String[]> currencyCheck(){
         ArrayList<String[]> pairs = new ArrayList<>();
         String[] currencies = {"US Dollar", "Canadian Dollar", "British Pound", "Euro", "Swiss Franc", "Australian Dollar"};
@@ -35,5 +46,19 @@ public class BlackBoxTests {
         return pairs.stream();
     }
 
+    /**
+     * This tests for values in [0, 1 000 000].
+     * We assume, if we try values outside of this threshold, we get a null value.
+     */
+    @ParameterizedTest
+    @ValueSource(doubles={-500d,-1d, 0, 500000d, 1000000d, 1000001d})
+    public void testCorrectAmount(Double amount){
+        if(amount <= 1000000 && amount >= 0){
+            assertEquals(Currency.convert(amount, 1d),amount);
+        }
+        else{
+            assertNull(Currency.convert(amount, 1d));
+        }
+    }
 
 }
